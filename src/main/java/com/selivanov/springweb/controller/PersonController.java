e69d4e2778077f0b1631d3472bf3d8e650a05a84
ilya03071993 <celiwan@mail.ru>
2023-12-15 17:55:53 +0300

    Fix some bags and add views

--- src/main/java/com/selivanov/springweb/controller/PersonController.java
+++ src/main/java/com/selivanov/springweb/controller/PersonController.java
@@ -10,10 +10,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class PersonController {
-    private Person person;
+    private List<Person> personList = new ArrayList<>();
 
     //1.
 //    @GetMapping("/param")
@@ -30,34 +31,74 @@ public class PersonController {
 //    }
 
     //2.
-//        @GetMapping("/get-form")
-//    public String getPersonRequest(Model model) {
-//        model.addAttribute("person", new Person());
-//        return "get-form";
-//    }
-//
-//    @GetMapping("/get-person")
-//    public String getPerson(@ModelAttribute Person person
-//            ,Model model) {
-//        model.addAttribute("person", person);
-//        return "getPerson";
-//    }
+        @GetMapping("/get-form")
+    public String getPersonRequest(Model model) {
+        model.addAttribute("person", new Person());
+        return "get-form";
+    }
+
+    @GetMapping("/get-person")
+    public String getPerson(@ModelAttribute Person person
+            ,Model model) {
+        model.addAttribute("person", person);
+        return "getPerson";
+    }
     //3.
-    @GetMapping("/form-person")
+    @GetMapping("/post-form")
     public String getPersonForm(Model model) {
         model.addAttribute("person", new Person());
-        return "formPerson";
+        return "post-form";
     }
+
     @GetMapping("/person")
-    public String createPerson(Model model) {
-        model.addAttribute("persons", List.of(person));
+    public String showPerson(@RequestParam(defaultValue = "5") Integer limit,
+                             @RequestParam(defaultValue = "age") String sortBy,
+                             @RequestParam(defaultValue = "asc") String sortOrder,
+                             @RequestParam(required = false) String filterField,
+                             @RequestParam(required = false) String filterValue,
+                             Model model) {
+        List<Person> sortedList = personList.stream().sorted((p1, p2) -> {
+                    if (sortOrder.equalsIgnoreCase("asc")) {
+                        if (sortBy.equals("age")) {
+                            return Integer.compare(p1.getAge(), p2.getAge());
+                        } else {
+                            return p1.getName().compareTo(p2.getName());
+                        }
+                    } else {
+                        if (sortBy.equals("age")) {
+                            return Integer.compare(p2.getAge(), p1.getAge());
+                        } else {
+                            return p2.getName().compareTo(p1.getName());
+                        }
+                    }
+                })
+                .limit(limit)
+                .collect(Collectors.toList());
+
+//        if (filterField.equalsIgnoreCase("gender") && filterValue.equalsIgnoreCase("male")){
+//            sortedList.removeIf(person -> person.getGender().equalsIgnoreCase("male"));
+//        } else if (filterField.equalsIgnoreCase("gender") && filterValue.equalsIgnoreCase("female")){
+//            sortedList.removeIf(person -> person.getGender().equalsIgnoreCase("female"));}
+
+        model.addAttribute("persons", sortedList);
         return "person";
     }
 
     @PostMapping("/person")
-    public String register(@ModelAttribute Person person,
-                           Model model) {
-        this.person = person;
+    public String register(@ModelAttribute Person person) {
+        personList.add(person);
         return "redirect:/person";
+    }
+
+    @GetMapping("/delete-person")
+    public String deletePerson(Model model) {
+        model.addAttribute("filterField", "");
+        model.addAttribute("filterValue", "");
+        return "deletePerson";
+    }
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
     }
 }
--- src/main/java/com/selivanov/springweb/model/Person.java
+++ src/main/java/com/selivanov/springweb/model/Person.java
@@ -1,5 +1,7 @@
 package com.selivanov.springweb.model;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private String gender;
@@ -35,4 +37,19 @@ public class Person {
     public void setAge(Integer age) {
         this.age = age;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(gender, person.gender) && Objects.equals(age, person.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, gender, age);
+    }
+
+
 }
